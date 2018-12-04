4346aa9df46b37ebad5aee203ba51e9402e5aa78
Leioy <13902303081@139.com>
2018-12-04 15:23:26 +0800

    添加button-group

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template lang="pug">
   #app
-    y-icon(name="settings")
+    y-button(icon="settings") 按钮
     y-button(:loading="true") 按钮
-    y-button(iconPosition="right" icon="settings") 按钮
+    y-button-group
+      y-button(icon="left") 上一页
+      y-button(icon="right" iconPosition="right") 下一页
 </template>
 
 <script>
 import YIcon from './components/icon/src/icon'
 import YButton from './components/button/src/button'
+import YButtonGroup from './components/button/src/button-group'
 export default {
   name: 'app',
   components: {
     YIcon,
     YButton,
+    YButtonGroup,
   },
 }
 </script>
---
+++ src/components/button/src/button-group.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  .y-button-group
+    slot
+</template>
+
+<script>
+export default {
+  name: 'y-button-group',
+  mounted () {
+    console.log(this.$el.children)
+    for (const node of this.$el.children) {
+      if (node.nodeName.toLocaleLowerCase() !== 'button') {
+        console.warn(`y-button-group的子元素应全为button，但你的写的是${node.nodeName}`)
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/scss/var";
+  .y-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    .y-button {
+      border-radius: 0;
+      &:not(first-child) {
+        margin-left: -1px;
+      }
+      &:first-child {
+        border-top-left-radius: $border-radius;
+        border-bottom-left-radius: $border-radius;
+      }
+      &:last-child {
+        border-top-right-radius: $border-radius;
+        border-bottom-right-radius: $border-radius;
+      }
+      // 为什么要添加这个样式，试试把$border-color-hover改成red就知道了
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
