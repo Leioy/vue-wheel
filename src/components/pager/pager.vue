45dec5a87a85deaccc738e9291f5bdaef3c7ad32
Leioy <13902303081@139.com>
2019-02-18 18:10:41 +0800

    完善pager功能

--- src/components/pager/pager.vue
+++ src/components/pager/pager.vue
@@ -1,13 +1,13 @@
 <template>
   <div :class="`${prefix}`">
-    <span :class="jumpPrevClass">
+    <span :class="jumpPrevClass" @click="onClickItem(currentPage - 1)">
       <y-icon name="left"></y-icon>
     </span>
     <template v-for="(page,index) in pages">
       <span v-if="page === '...'" :key="index" :class="`${prefix}-separator`">{{page}}</span>
-      <span v-else :key="index" :class="classObj(page)">{{page}}</span>
+      <span v-else :key="index" :class="classObj(page)" @click="onClickItem(page)">{{page}}</span>
     </template>
-    <span :class="jumpNextClass">
+    <span :class="jumpNextClass" @click="onClickItem(currentPage + 1)">
       <y-icon name="right"></y-icon>
     </span>
   </div>
@@ -43,19 +43,21 @@ export default {
     },
   },
   data () {
-    let pages = [1, this.totalPage, this.currentPage, this.currentPage - 1, this.currentPage - 2, this.currentPage + 1, this.currentPage + 2]
-    const temp = uniqueArray(pages.sort((a, b) => a - b)).filter(page => page >= 1 && page <= this.totalPage)
-    const newPages = temp.reduce((prev, current, index) => {
-      prev.push(current)
-      temp[index + 1] && temp[index + 1] - temp[index] > 1 && prev.push('...')
-      return prev
-    }, [])
     return {
       prefix,
-      pages: newPages,
     }
   },
   computed: {
+    pages () {
+      let pages = [1, this.totalPage, this.currentPage, this.currentPage - 1, this.currentPage - 2, this.currentPage + 1, this.currentPage + 2]
+      const temp = uniqueArray(pages.sort((a, b) => a - b)).filter(page => page >= 1 && page <= this.totalPage)
+      const newPages = temp.reduce((prev, current, index) => {
+        prev.push(current)
+        temp[index + 1] && temp[index + 1] - temp[index] > 1 && prev.push('...')
+        return prev
+      }, [])
+      return newPages
+    },
     jumpPrevClass () {
       return [
         `${this.prefix}-jump-prev`,
@@ -70,7 +72,12 @@ export default {
     },
   },
   methods: {
-    onClickItem () {},
+    onClickItem (page) {
+      if (page >= 1 && page <= this.totalPage) {
+        this.$emit('update:currentPage', page)
+        this.$emit('on-change', page)
+      }
+    },
     classObj (page) {
       return [
         `${this.prefix}-item`,
