d39f892bff8747bdd0884970084027aee97beb16
Leioy <13902303081@139.com>
2019-03-07 16:51:12 +0800

    初步完成sticky

---
+++ src/components/sticky/sticky.vue
@@ -0,0 +1,84 @@
+<template>
+  <div :class="`${prefix}-wrapper`">
+    <div :class="classObj" :style="styleObj">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+const prefix = 'y-sticky'
+
+function getWindowScroll (top) {
+  const prop = top ? 'pageYOffset' : 'pageXOffset'
+  const method = top ? 'scrollTop' : 'scrollLeft'
+  let ret = window[prop]
+
+  // 考虑不兼容pageYoffset的情况
+  if (typeof ret !== 'number') {
+    ret = document.documentElement[method]
+  }
+  return ret
+}
+
+function getComputedOffset (el) {
+  const rect = el.getBoundingClientRect()
+  const scrollTop = getWindowScroll(true)
+  const scrollLeft = getWindowScroll()
+  const docEl = document.body
+
+  return {
+    top: rect.top + scrollTop - docEl.clientTop,
+    left: rect.left + scrollLeft - docEl.clientLeft,
+  }
+}
+export default {
+  name: prefix,
+  props: {
+    offset: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data () {
+    return {
+      prefix,
+      sticky: false,
+      styleObj: {},
+    }
+  },
+  computed: {
+    classObj () {
+      return [
+        `${prefix}`,
+        {
+          [`${prefix}-active`]: this.sticky,
+        },
+      ]
+    },
+  },
+  mounted () {
+    window.addEventListener('scroll', this.scrollHandler)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.scrollHandler)
+  },
+  methods: {
+    scrollHandler () {
+      const scrollTop = getWindowScroll(true)
+      const elOffset = getComputedOffset(this.$el)
+      if (elOffset.top - this.offset < scrollTop) {
+        this.sticky = true
+        this.styleObj = {
+          top: `${this.offset}px`,
+          left: `${elOffset.left}px`,
+          width: `${this.$el.offsetWidth}px`,
+        }
+      } else {
+        this.sticky = false
+        this.styleObj = {}
+      }
+    },
+  },
+}
+</script>
