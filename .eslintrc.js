eff6e4cee0ef46b3895f541c9adf0b21e5240584
Leioy <13902303081@139.com>
2019-03-23 18:00:26 +0800

    button组件单元测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'always-multiline'],
+    'no-unused-expressions': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
