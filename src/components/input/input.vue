371059f8f4f1516b4f7e545188942172761b953d
Leioy <13902303081@139.com>
2019-03-27 15:51:04 +0800

    添加input

---
+++ src/components/input/input.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="`${prefix}-wrapper`">
+    <input type="text"
+    :disabled="disabled"
+    :value="value"
+    @change="$emit('change',$event.target.value)"
+    @blur="$emit('blur',$event.target.value)"
+    @input="$emit('input',$event.target.value)"
+    @focus="$emit('focus',$event.target.value)"
+    >
+    <template v-if="error">
+      <y-icon name="error" class="icon-error"></y-icon>
+      <span class="errorMessage">{{error}}</span>
+    </template>
+  </div>
+</template>
+
+<script>
+import YIcon from '../icon/icon'
+const prefix = 'y-input'
+export default {
+  name: prefix,
+  components: {
+    YIcon,
+  },
+  props: {
+    value: {},
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    error: {
+      type: String,
+    },
+  },
+  data () {
+    return {
+      prefix,
+    }
+  },
+}
+</script>
