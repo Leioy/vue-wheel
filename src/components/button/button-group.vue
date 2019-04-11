a12df2ceb39cc91200fde0f137a7b14e3d7a0637
Leioy <13902303081@139.com>
2019-04-12 02:51:30 +0800

    fix button-group

--- src/components/button/button-group.vue
+++ src/components/button/button-group.vue
@@ -9,7 +9,7 @@ export default {
   name: 'y-button-group',
   mounted () {
     for (const node of this.$el.children) {
-      if (node.nodeName.toLocaleLowerCase() !== 'button') {
+      if (node && node.nodeName.toLocaleLowerCase() !== 'button') {
         console.warn(`y-button-group的子元素应全为button，但你的写的是${node.nodeName}`)
       }
     }
