13363302eb29daf3aa45c6e92cc1dbca29f78bcb
Leioy <13902303081@139.com>
2019-04-12 02:58:21 +0800

    lib模式的入口文件和npmignore

--- src/index.js
+++ src/index.js
@@ -1,22 +1,73 @@
 import './assets/styles/index.scss'
-export { default as Button } from './components/button/button.vue'
-export { default as ButtonGroup } from './components/button/button-group.vue'
-export { default as CascaderItem } from './components/cascader/cascader-item.vue'
-export { default as CascaderPane } from './components/cascader/cascader-pane.vue'
-export { default as Cascader } from './components/cascader/cascader.vue'
-export { default as Col } from './components/col/col.vue'
-export { default as CollapseItem } from './components/collapse/collapse-item.vue'
-export { default as Collapse } from './components/collapse/collapse.vue'
-export { default as DatePicker } from './components/date-picker/date-picker'
-export { default as Icon } from './components/icon/icon.vue'
-export { default as Mixin } from './components/menu/mixin'
-export { default as MenuItem } from './components/menu/menu-item'
-export { default as SubMenu } from './components/menu/sub-menu'
-export { default as Menu } from './components/menu/menu'
-export { default as Message } from './components/message/message'
-export { default as Pager } from './components/pager/pager.vue'
-export { default as Popover } from './components/popover/popover.vue'
-export { default as Row } from './components/row/row.vue'
-export { default as Sticky } from './components/sticky/sticky.vue'
-export { default as Pane } from './components/tabs/pane.vue'
-export { default as Tabs } from './components/tabs/tabs.vue'
+import './assets/icons/index'
+import Button from './components/button/button.vue'
+import ButtonGroup from './components/button/button-group.vue'
+import CascaderItem from './components/cascader/cascader-item.vue'
+import CascaderPane from './components/cascader/cascader-pane.vue'
+import Cascader from './components/cascader/cascader.vue'
+import Col from './components/col/col.vue'
+import CollapseItem from './components/collapse/collapse-item.vue'
+import Collapse from './components/collapse/collapse.vue'
+import DatePicker from './components/date-picker/date-picker'
+import Icon from './components/icon/icon.vue'
+import MenuItem from './components/menu/menu-item'
+import SubMenu from './components/menu/sub-menu'
+import Menu from './components/menu/menu'
+import Message from './components/message/index'
+import Pager from './components/pager/pager.vue'
+import Popover from './components/popover/popover.vue'
+import Row from './components/row/row.vue'
+import Sticky from './components/sticky/sticky.vue'
+import Pane from './components/tabs/pane.vue'
+import Tabs from './components/tabs/tabs.vue'
+
+const components = [
+  Button,
+  ButtonGroup,
+  CascaderItem,
+  CascaderPane,
+  Cascader,
+  Col,
+  CollapseItem,
+  Collapse,
+  DatePicker,
+  Icon,
+  MenuItem,
+  SubMenu,
+  Menu,
+  Pager,
+  Popover,
+  Row,
+  Sticky,
+  Pane,
+  Tabs,
+]
+const install = Vue => {
+  components.forEach(component => {
+    Vue.component(component.name, component)
+  })
+  Vue.use(Message)
+}
+export default {
+  install,
+  Button,
+  ButtonGroup,
+  CascaderItem,
+  CascaderPane,
+  Cascader,
+  Col,
+  CollapseItem,
+  Collapse,
+  DatePicker,
+  Icon,
+  MenuItem,
+  SubMenu,
+  Menu,
+  Pager,
+  Popover,
+  Row,
+  Sticky,
+  Pane,
+  Tabs,
+  Message,
+}
