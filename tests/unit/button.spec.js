2dc2a61ae381d95e49c701dc2fd357cb1ec670ca
Leioy <13902303081@139.com>
2019-04-11 15:28:01 +0800

    fix button测试用例

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -30,31 +30,6 @@ describe('Button.vue', () => {
     expect(useElements.length).to.equal(1)
     expect(useElements[0].getAttribute('xlink:href')).to.equal('#icon-loading')
   })
-
-  xit('icon 默认的 order 是 1', () => {
-    const wrapper = mount(Button, {
-      attachToDocument: true,
-      propsData: {
-        icon: 'settings',
-      },
-    })
-    const vm = wrapper.vm
-    const icon = vm.$el.querySelector('svg')
-    expect(getComputedStyle(icon).order).to.eq('1')
-  })
-
-  xit('设置 iconPosition 可以改变 order', () => {
-    const wrapper = mount(Button, {
-      attachToDocument: true,
-      propsData: {
-        icon: 'settings',
-        iconPosition: 'right',
-      },
-    })
-    const vm = wrapper.vm
-    const icon = vm.$el.querySelector('svg')
-    expect(getComputedStyle(icon).order).to.eq('2')
-  })
   it('点击 button 触发 click 事件', () => {
     const wrapper = mount(Button, {
       propsData: {
